4846af55764538f61b2000d8447b493da79aa635
BelalGalhom <113742499+BelalGalhom@users.noreply.github.com>
2025-08-28 18:56:23 +0300

    Burn effect

--- code/game/g_main.c
+++ code/game/g_main.c
@@ -321,6 +321,7 @@ vmCvar_t    g_automsgPeriodicity; // How often we send an automessage. This does
 vmCvar_t    g_autotipsPeriodicity;
 
 vmCvar_t    g_ladderType;
+vmCvar_t    g_BurnEffect;
 
 static cvarTable_t gameCvarTable[] =
 {
@@ -652,6 +653,7 @@ static cvarTable_t gameCvarTable[] =
     { &g_automsgPeriodicity, "g_automsgPeriodicity", "3", CVAR_ARCHIVE | CVAR_LOCK_RANGE, 1.0f, 30.0f, 0, qfalse },
     { &g_autotipsPeriodicity, "g_autotipsPeriodicity", "5", CVAR_ARCHIVE | CVAR_LOCK_RANGE, 1.0f, 30.0f, 0, qfalse },
     { &g_ladderType, "g_ladderType", "0", CVAR_ARCHIVE, 0.0f, 0.0f, 0, qfalse },
+    { &g_BurnEffect, "g_BurnEffect", "0", CVAR_ARCHIVE, 0.0f, 0.0f, 0, qfalse },
 };
 
 // bk001129 - made static to avoid aliasing
