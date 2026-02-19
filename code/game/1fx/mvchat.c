4b1de022705d428f2e318c13c3b7e1144a6f4313
Janno Esko <JannoEsko@users.noreply.github.com>
2026-02-19 21:02:46 +0200

    fix: Zero-init mvchatSound->sounds with the correct size
    The sounds element was zeroed as a struct array instead of pointer array. Fixes issue #26

--- code/game/1fx/mvchat.c
+++ code/game/1fx/mvchat.c
@@ -229,7 +229,7 @@ void mvchat_parseFiles()
 
             // Zero-initialize the sounds pointer array with the
             // number of languages available.
-            Com_Memset(mvchatSound->sounds, 0, sizeof(mvchatSoundLang_t) * MVCHAT_NUM_LANGS);
+            Com_Memset(mvchatSound->sounds, 0, sizeof(mvchatSound->sounds));
 
             // Strip extra color escapes from the text.
             G_RemoveAdditionalCarets(sText);
